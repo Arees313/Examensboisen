a51ac521c1ea1e10bc46b137677a407cc0348c66
Ali B <alibajalan@hotmail.com>
2024-11-15 18:04:41 +0100

    added functions

--- Api_coin.py
+++ Api_coin.py
@@ -26,15 +26,18 @@ if response.status_code == 200:
             current_price = coin['current_price']
             max_supply = coin.get('max_supply', 'N/A')  # Handle None value for max_supply
             circulating_supply = coin.get('circulating_supply', 'N/A')  # Handle None value for circulating_supply
-            price_change_24h = coin['price_change_percentage_24h']
-
+            price_change_procentage_24h = coin['price_change_percentage_24h']
+            prince_change_USD_24h = coin ['price_change_24h']
             # Colorize percentage change (positive = green, negative = red)
-            if price_change_24h > 0:
-                price_change_24h = f"{Fore.GREEN}{price_change_24h}%{Style.RESET_ALL}"
+            if price_change_procentage_24h > 0:
+                price_change_procentage_24h = f"{Fore.GREEN}{price_change_procentage_24h}%{Style.RESET_ALL}"
             else:
-                price_change_24h = f"{Fore.RED}{price_change_24h}%{Style.RESET_ALL}"
-
-            print(f"{index}. Name:{Style.BRIGHT} {name}{Style.RESET_ALL}, Price: ${current_price} USD, Volume: {total_volume}, Max Available: {max_supply}, Circulating Supply: {circulating_supply}, 24h Change: {price_change_24h}")
+                price_change_procentage_24h = f"{Fore.RED}{price_change_procentage_24h}%{Style.RESET_ALL}"
+            if prince_change_USD_24h > 0:
+                prince_change_USD_24h = f"{Fore.GREEN}{prince_change_USD_24h}${Style.RESET_ALL}"
+            else:
+                prince_change_USD_24h = f"{Fore.RED}{prince_change_USD_24h}${Style.RESET_ALL}"
+            print(f"{index}. Name:{Style.BRIGHT} {name}{Style.RESET_ALL}, Price: ${current_price} USD, Volume: {total_volume}, Max Available: {max_supply}, Circulating Supply: {circulating_supply}, 24h Change: {price_change_procentage_24h}, 24h change in USD: {prince_change_USD_24h}")
         
         # Sleep for a desired time (e.g., 10 seconds) before refreshing the print
         sleep(10)
